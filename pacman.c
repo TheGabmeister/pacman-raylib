42d5ae4fe574d3a9a7a5e5517572806ce86ac646
TheGabmeister <thegabmeister@protonmail.com>
2025-04-16 20:40:12 +0800

    Draw Pacman at center

--- pacman.c
+++ pacman.c
@@ -14,12 +14,29 @@ Sound fxCoin = { 0 };
 static const int screenWidth = 600;
 static const int screenHeight = 800;
 
+typedef struct {
+    Vector2 position;   // Pacman's position in pixels
+    Vector2 direction;  // Current movement direction
+    float speed;        // Movement speed (pixels per frame)
+    float radius;       // For drawing Pacman
+} Pacman;
+
+// Initialize Pacman at a given position
+Pacman InitPacman(float x, float y)
+{
+    Pacman p;
+    p.position = (Vector2){ x, y };
+    p.direction = (Vector2){ 0, 0 };
+    p.speed = 2.0f;
+    p.radius = 20.0f;
+    return p;
+}
+
+Pacman pacman;
+
 // Local Functions Declaration
 static void UpdateDrawFrame(void);          // Update and draw one frame
 
-//----------------------------------------------------------------------------------
-// Main entry point
-//----------------------------------------------------------------------------------
 int main(void)
 {
     // Initialization
@@ -28,6 +45,8 @@ int main(void)
 
     InitAudioDevice();      // Initialize audio device
 
+    pacman = InitPacman(screenWidth / 2.0f, screenHeight / 2.0f);
+    
     // Load global data (assets that must be available in all screens, i.e. font)
     font = LoadFont("resources/mecha.png");
     //music = LoadMusicStream("resources/ambient.ogg"); // TODO: Load music
@@ -40,7 +59,6 @@ int main(void)
     emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
 #else
     SetTargetFPS(60);       // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
@@ -57,7 +75,6 @@ int main(void)
     CloseAudioDevice();     // Close audio context
 
     CloseWindow();          // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
 
     return 0;
 }
@@ -68,16 +85,30 @@ static void UpdateDrawFrame(void)
     // Update
     //----------------------------------------------------------------------------------
     //UpdateMusicStream(music);       // NOTE: Music keeps playing between screens
-
     
+    // Keyboard input for Pacman movement
+    if (IsKeyDown(KEY_RIGHT)) pacman.direction = (Vector2){ 1, 0 };
+    else if (IsKeyDown(KEY_LEFT)) pacman.direction = (Vector2){ -1, 0 };
+    else if (IsKeyDown(KEY_UP)) pacman.direction = (Vector2) { 0, -1 };
+    else if (IsKeyDown(KEY_DOWN)) pacman.direction = (Vector2) { 0, 1 };
+
+    // Update Pacman's position
+    pacman.position.x += pacman.direction.x * pacman.speed;
+    pacman.position.y += pacman.direction.y * pacman.speed;
+
+
     // Draw
     //----------------------------------------------------------------------------------
     BeginDrawing();
+    ClearBackground(BLACK);
 
-        ClearBackground(RAYWHITE);
+    // Draw Pacman as a yellow circle
+    DrawCircleV(pacman.position, pacman.radius, YELLOW);
 
-        //DrawFPS(10, 10);
+
+    //DrawFPS(10, 10);
 
     EndDrawing();
     //----------------------------------------------------------------------------------
 }
+
